00bbe85cce64defa06838e92541084338f328a1f
685288 <jayachandran.thangeswaran@cognizant.com>
2018-05-28 22:46:29 +0530

    Initial Commit

---
+++ src/main/java/com/college/timetable/AppConstants.java
@@ -0,0 +1,9 @@
+package com.college.timetable;
+
+public class AppConstants {
+    public enum ESubjectNames{
+        PHYSICS, CHEMISTRY, MATHS
+    }
+
+    public static final String CONCAT_CHAR="_";
+}
